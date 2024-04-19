d05446b919821f3dd90c63d9ab159102780a4d2d
MurataSakuya <144077681+Muratasakuya@users.noreply.github.com>
2024-04-19 10:21:57 +0900

    不要なコメントアウトの削除

--- Entities/Grid/Grid.h
+++ Entities/Grid/Grid.h
@@ -20,10 +20,5 @@ public:
 	// デストラクタ
 	~Grid() {}
 
-	// グリッド線を表示する関数
-	/*void DrawGid(
-		const Matrix4x4& viewProjectionMatrix, const Matrix4x4& viewportMatrix, const Vec3f& startPos, const Vec3f& endPos);*/
-
-	void DrawGridLine(const Matrix4x4& viewProjectionMatrix, const Matrix4x4& viewportMatrix, const Vec3f& startPos, const Vec3f& endPos);
 	void DrawGrid(const Matrix4x4& viewMatrix, const Matrix4x4& projectionMatrix, const Matrix4x4& viewportMatrix);
 };
